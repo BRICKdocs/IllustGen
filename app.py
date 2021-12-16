423676a46f01051cc2349b1d820ac3dd35ede83f
jhshin9345@gmail.com <jhshin9345@gmail.com>
2021-12-16 22:09:39 +0900

    init app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+from logging import debug
+from flask import Flask, render_template
+from werkzeug import redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    render_template("index.html")
+
+
+@app.route("/loading")
+def generate_loading():
+    pass
+
+@app.route("/post")
+def post_hub():
+    # all page redirect works via this function
+    pass
+
+
+@app.route("/input")
+def input_data():
+    pass
+
+def minting():
+    pass
+
+@app.route("/result")
+def generate_img():
+    pass
+
+def generate_nft():
+    pass
+
+if __name__ == '__main__':
+    app.run(port=5050, debug=True)
