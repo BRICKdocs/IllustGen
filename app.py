7342508f96d52729d98ba95197549dc6975e7394
Brickdocs ShinJunho <jhshin9345@gmail.com>
2021-12-21 13:18:04 +0900

    updated post predict

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from logging import debug
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask.wrappers import Request
 from werkzeug.utils import redirect
+from torch_utils import transform_image, get_prediction
 # from werkzeug import redirect
 
 app = Flask(__name__)
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+def allowed_file(filename):
+    # xxx.png
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files.get('file')
+        if file is None or file.filename == "":
+            return jsonify({'error': 'no file'})
+        if not allowed_file(file.filename):
+            return jsonify({'error': 'format not supported'})
+
+        try:
+            img_bytes = file.read()
+            tensor = transform_image(img_bytes)
+            prediction = get_prediction(tensor)
+            data = {'prediction': prediction.item(), 'class_name': str(prediction.item())}
+            return jsonify(data)
+        except:
+            return jsonify({'error': 'error during prediction'})
 
 @app.route("/")
 def index():
