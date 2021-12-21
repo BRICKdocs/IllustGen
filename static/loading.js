ea57687985fa3caa0e7c830d664ce32a33c69eb7
jhshin9345@gmail.com <jhshin9345@gmail.com>
2021-12-21 22:31:06 +0900

    fixed bug

--- static/loading.js
+++ static/loading.js
@@ -1,6 +1,6 @@
 function loading (){
     var loading = document.getElementsByClassName("loading");
-    var container = document.getElementsByClassName("container");
+    var container = document.getElementsByClassName("result-container");
     loading[0].style.display = 'none';
     container[0].style.display = 'block';
 }
