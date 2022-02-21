a7572874c1b8fc0b0dc009bc00fd3d12c26f436e
jhshin9345@gmail.com <jhshin9345@gmail.com>
2022-02-21 22:02:52 +0900

    infura connect test

--- app/Etherium/eth-tester.py
+++ app/Etherium/eth-tester.py
@@ -1,3 +1,6 @@
 from web3 import Web3, EthereumTesterProvider
-w3 = Web3(EthereumTesterProvider())
-w3.isConnected()
+import web3
+infura_url = "https://palm-testnet.infura.io/v3/2bd1199ab4664e95affacc45827de31f"
+# w3 = Web3(EthereumTesterProvider())
+web3 = Web3(Web3.HTTPProvider(infura_url))
+print(web3.isConnected())
