97ee4a15c74acc82c3f0edacba5a7804385a8058
jhshin9345@gmail.com <jhshin9345@gmail.com>
2022-02-21 22:06:57 +0900

    eth beginning

--- app/Etherium/eth-tester.py
+++ app/Etherium/eth-tester.py
@@ -3,4 +3,6 @@ import web3
 infura_url = "https://palm-testnet.infura.io/v3/2bd1199ab4664e95affacc45827de31f"
 # w3 = Web3(EthereumTesterProvider())
 web3 = Web3(Web3.HTTPProvider(infura_url))
-print(web3.isConnected())
+# print(web3.isConnected()) # Boolean testNet connect  
+# print(web3.eth.blockNumber) # Print blockNumber
+
