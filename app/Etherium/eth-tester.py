cf13a6fec039468a97f01a8f77eb19a3eb5cde66
jhshin9345@gmail.com <jhshin9345@gmail.com>
2022-02-21 22:57:51 +0900

    updated

--- app/Etherium/eth-tester.py
+++ app/Etherium/eth-tester.py
@@ -1,8 +1,19 @@
 from web3 import Web3, EthereumTesterProvider
-import web3
+import json
 infura_url = "https://palm-testnet.infura.io/v3/2bd1199ab4664e95affacc45827de31f"
 # w3 = Web3(EthereumTesterProvider())
 web3 = Web3(Web3.HTTPProvider(infura_url))
-# print(web3.isConnected()) # Boolean testNet connect  
-# print(web3.eth.blockNumber) # Print blockNumber
+# print(web3.isConnected()) # Check testNet connect  
+
+
+# plz go to ethscan
+abi = "" #smart contract looks okays
+address = "" #deployed smart contract 
+
+contract = web3.eth.contract(address=address, abi=abi)
+totalSupply = contract.functions.totalSupply().call()
+
+
+
+
 
